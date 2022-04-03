82f1a0c70ffa89987d15bfbc3d25d4e2e259e941
dudaslevente <dudaslevente@turr.hu>
2022-04-03 12:16:17 +0200

    Javítás

--- feladat3.py
+++ feladat3.py
@@ -1,49 +1,59 @@
-from tkinter import * 
+import math
+from tkinter import *
+
+from setuptools import Command 
 foablak = Tk() 
 def terfogat(): 
-    r = int(elsomezo.get()) 
-    m = int(masodik.get()) 
-    c = 3.14*r*r*m 
-    negyedik.delete(0, END) 
-    negyedik.insert(0, ''+str(c)+'cm2') 
+    r = float(elsomezo.get()) 
+    m = float(masodikmezo.get()) 
+    c = math.pi*r*r*m 
+    negyedikmezo.delete(0, END) 
+    negyedikmezo.insert(0, ''+str("%.0f" % c)) 
+    
 
 def liter(): 
-    a=int(elsomezo.get()) 
-    b=(Hordo) 
+    a=float(elsomezo.get()) 
+    b=float(negyedikmezo.get()) 
     c=b-a 
-    
+    otodikmezo.delete(0, END) 
+    otodikmezo.insert(0, ''+str("%.0f" % c))
+
 
 
 elso=Label(foablak, text='Hány liter?', ) 
 elso.grid(row=1, column=1) 
 elsomezo=Entry(foablak) 
 elsomezo.grid(row=1, column=2) 
-masodikmezo=Label(foablak, text='Milyen hosszú a sugara? (cm)') 
-masodikmezo.grid(row=2, column=1) 
-masodik=Entry(foablak) 
-masodik.grid(row=2, column=2)
-harmadikmezo=Label(foablak, text='Milyen magas a hordó? (cm)') 
-harmadikmezo.grid(row=3, column=1) 
-harmadik=Entry(foablak) 
-harmadik.grid(row=3, column=2) 
+masodik=Label(foablak, text='Milyen hosszú a sugara? (cm)') 
+masodik.grid(row=2, column=1) 
+masodikmezo=Entry(foablak) 
+masodikmezo.grid(row=2, column=2)
+harmadik=Label(foablak, text='Milyen magas a hordó? (cm)') 
+harmadik.grid(row=3, column=1) 
+harmadikmezo=Entry(foablak) 
+harmadikmezo.grid(row=3, column=2) 
 gomb=Button(foablak, text='Kiszámít', command=terfogat) 
 gomb.grid(row=4, column=2) 
+'''
 eredmeny=Label(foablak, text='Térfogat') 
 eredmeny.grid(row=5, column=1) 
 eredmenymezo=Entry(foablak) 
 eredmenymezo.grid(row=5, column=2) 
-negyedik=Entry(foablak)
-negyedik.grid(row=6, column=2) 
-gomb2=Label(foablak, text='Liter') 
-gomb2.grid(row=6, column=1) 
-Hordo=50 
-otodik.delete(0, END) 
-otodik.insert(0, 'Ennyi fér még bele: '+str(c)) 
-otodik=Entry(foablak) 
-otodik.grid(row=4, column=1)
+'''
+negyedikmezo=Entry(foablak)
+negyedikmezo.grid(row=5, column=2) 
+negyedik=Label(foablak, text='Hordó (l)') 
+negyedik.grid(row=5, column=1) 
+#Hordo=50 
+otodik=Label(foablak, text='Mennyi fér még bele? (l)') 
+otodik.grid(row=6, column=1)
+otodikmezo=Entry(foablak) 
+otodikmezo.grid(row=6, column=2)
 
 gomb3=Button(foablak, text='Kivonás', command=liter) 
-gomb3.grid(row=8, column=2) 
+gomb3.grid(row=9, column=2) 
+
+
 foablak.mainloop()
 
 
