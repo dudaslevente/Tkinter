f99bfabe8d3d01876002938cf076e2c6c36aa6e2
dudaslevente <78853751+dudaslevente@users.noreply.github.com>
2022-04-06 13:24:37 +0200

    Menü

---
+++ feladat6.py
@@ -0,0 +1,92 @@
+from tkinter import *
+
+def nevjegy():
+    abl2=Toplevel(foablak)
+    uz2=Message(abl2, text='Készítette: Dudás Levente\nDusnok\n2005.12.09', width=300)
+    gomb2=Buttom(abl2, texz='Kilép', command=abl2.destory)
+    uz2.pack()
+    gomb2.pack()
+    abl2.pack()
+
+def felszin():
+
+    def szamit():
+        a=eval(mezo1.get())
+        b=eval(mezo2.get())
+        c=eval(mezo3.get())
+        felszin=2*(a*b+a*c+b*c)
+        mezo4.delete(0,End)
+        mezo4.insert(0,str(felszin))
+    
+    abl3=Toplevel(foablak)
+    abl3.title('A téglatest felszíne')
+    abl3.minsize(width=300, height=100)
+    szoveg1=Label(abl3, text='a:')
+    szoveg2=Label(abl3, text='b:')
+    szoveg3=Label(abl3, text='c:')
+    szoveg4=Label(abl3, text='Eredmény:')
+    gomb1=Button(abl3, text='Számítás', command=szamit)
+    mezo1=Entry(abl3)
+    mezo2=Entry(abl3)
+    mezo3=Entry(abl3)
+    mezo4=Entry(abl3)
+    szoveg1.grid(row=1)
+    szoveg2.grid(row=2)
+    szoveg3.grid(row=3)
+    szoveg4.grid(row=5)
+    gomg1.grid(row=4, column=2, sticky=W)
+    mezo1.grid(row=1, column=2, sticky=W)
+    mezo2.grid(row=2, column=2, sticky=W)
+    mezo3.grid(row=3, column=2, sticky=W)
+    mezo4.grid(row=5, column=2, sticky=W)
+    abl3.mainloop()
+
+def terfogat():
+
+    def szamit():
+        a=eval(mezo1.get())
+        b=eval(mezo2.get())
+        c=eval(mezo3.get())
+        felszin=a*b*c
+        mezo4.delete(0,End)
+        mezo4.insert(0,str(terfogat))
+
+    abl3=Toplevel(foablak)
+    abl3.title('A téglatest térfogata')
+    abl3.minsize(width=300, height=100)
+    szoveg1=Label(abl3, text='a:')
+    szoveg2=Label(abl3, text='b:')
+    szoveg3=Label(abl3, text='c:')
+    szoveg4=Label(abl3, text='Eredmény:')
+    gomb1=Button(abl3, text='Számítás', command=szamit)
+    mezo1=Entry(abl3)
+    mezo2=Entry(abl3)
+    mezo3=Entry(abl3)
+    mezo4=Entry(abl3)
+    szoveg1.grid(row=1)
+    szoveg2.grid(row=2)
+    szoveg3.grid(row=3)
+    szoveg4.grid(row=5)
+    gomg1.grid(row=4, column=2, sticky=W)
+    mezo1.grid(row=1, column=2, sticky=W)
+    mezo2.grid(row=2, column=2, sticky=W)
+    mezo3.grid(row=3, column=2, sticky=W)
+    mezo4.grid(row=5, column=2, sticky=W)
+    abl3.mainloop()
+
+foablak=Tk()
+foablak.title('A téglatest adatai')
+foablak.minsize(width=300, height=100)
+
+menusor=Frame(foablak)
+menusor.pack(side=TOP, fill=X)
+
+menu2=Menubutton(menusor, text='Féjl', underline=0)
+menu2.pack(side=LEFT)
+teglatest=Menu(menu2)
+teglatest.add_command(label='Felszín', command=felszin, underline=0)
+teglatest.add_command(label='Térfogat', command=terfogat, underline=0)
+menu2.config(menu=teglatest)
+
+foablak.mainloop()
+
